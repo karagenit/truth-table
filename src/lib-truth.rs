5d66d1b55a5f829f3cb0523a5a91ece7588a8029
Caleb Smith <karagenit@outlook.com>
2017-09-29 12:37:10 -0400

    Add IO Code

--- src/lib-truth.rs
+++ src/lib-truth.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 pub fn read_table() {
-    println!("Hello!");
+    println!("Enter Number of Variables: ");
+
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input)
+        .expect("Failed to read line");
+
+    println!("{}", input);
 }
