2fa0a6e7d866c7d37b5f0ec9bffad7878a43a4f5
Caleb Smith <karagenit@outlook.com>
2017-10-02 11:22:40 -0400

    Store Inputs in Vector

--- src/lib-truth.rs
+++ src/lib-truth.rs
@@ -9,15 +9,22 @@ pub fn read_table() {
     io::stdin().read_line(&mut input)
         .expect("Failed to read line");
 
-    let inputs: u32 = input.trim().parse().expect("Enter a number!");
+    let inputs: u32 = input.trim().parse::<u32>().expect("Enter a number!");
 
-    let combos = 2u32.pow(inputs);
+    let combos: usize = 2u32.pow(inputs) as usize;
+
+    let mut outputs = vec![false; combos].into_boxed_slice();
 
     for n in 0..combos {
         print!("|{}|", format!("{:0d$b}", n, d = (inputs as usize)));
         io::stdout().flush()
             .ok().expect("Cannot flush");
+
+        input = String::new(); //apparently read_line() appends to the string
         io::stdin().read_line(&mut input)
             .expect("Failed to read line");
+        outputs[n] = input.trim().parse::<u32>().expect("Enter 1 or 0") > 0;
     }
+
+    println!("{:?}", outputs);
 }
