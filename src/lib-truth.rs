b747ecf4b53b71eb867f38f72529f50aa40b1e68
Caleb Smith <karagenit@outlook.com>
2017-10-02 10:39:59 -0400

    Read Lines for Each Combo

--- src/lib-truth.rs
+++ src/lib-truth.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::io::prelude::*;
 
 pub fn read_table() {
     println!("Enter Number of Inputs: ");
@@ -13,6 +14,10 @@ pub fn read_table() {
     let combos = 2u32.pow(inputs);
 
     for n in 0..combos {
-        println!("|{}|?|", format!("{:0d$b}", n, d = (inputs as usize)));
+        print!("|{}|", format!("{:0d$b}", n, d = (inputs as usize)));
+        io::stdout().flush()
+            .ok().expect("Cannot flush");
+        io::stdin().read_line(&mut input)
+            .expect("Failed to read line");
     }
 }
