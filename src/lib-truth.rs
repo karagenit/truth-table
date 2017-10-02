cdc839311eeba00a574ef1a4092b7674bda32e90
Caleb Smith <karagenit@outlook.com>
2017-10-02 10:23:13 -0400

    Add Print Formatting

--- src/lib-truth.rs
+++ src/lib-truth.rs
@@ -10,7 +10,9 @@ pub fn read_table() {
 
     let var_cnt: u64 = input.trim().parse().expect("Enter a number!");
 
+    let digits = (((var_cnt - 1)as f64).log2() as usize) + 1;
+
     for n in 0..var_cnt {
-        println!("|{}|?|", format!("{:04b}", n));
+        println!("|{}|?|", format!("{:0d$b}", n, d = digits));
     }
 }
